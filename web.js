f2c35190affba4c3a5c8b4df0356bc33135c1168
Sam Bromley <sam@sambromley.com>
2013-07-05 22:04:13 -0230

    Require fs module.

--- web.js
+++ web.js
@@ -1,6 +1,7 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
+var fs = require('fs');
 
 app.get('/', function(request, response) {
 	var buf = fs.readFileSync('index.html');
