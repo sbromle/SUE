94e413a80efaee8e4653aa19bd75667567f28201
Sam Bromley <sam@sambromley.com>
2013-07-07 01:34:47 -0230

    Trying to clean up asynchronous bits.

--- grader.js
+++ grader.js
@@ -22,9 +22,11 @@ References:
 */
 
 var fs = require('fs');
+var restler = require('restler');
 var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
+var HTMLURL_DEFAULT = "";
 var CHECKSFILE_DEFAULT = "checks.json";
 
 var assertFileExists = function(infile) {
@@ -36,23 +38,80 @@ var assertFileExists = function(infile) {
     return instr;
 };
 
-var cheerioHtmlFile = function(htmlfile) {
-    return cheerio.load(fs.readFileSync(htmlfile));
+var assertURLExists = function(inurl) {
+    var instr = inurl.toString();
+		/*
+    if(!fs.existsSync(instr)) {
+        console.log("%s does not exist. Exiting.", instr);
+        process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
+    }
+		*/
+    return instr;
+};
+
+var printJson = function (checkJson) {
+ 	var outJson = JSON.stringify(checkJson, null, 4);
+ 	console.log(outJson);
+};
+
+/* loads data into cheerio instance. Checks it
+ * using data from checksfile,
+ * and then calls 'action' on the result.
+ */
+var cheerioLoadAndCheck = function(data,checksfile,action) {
+  $ = cheerio.load(data);
+	var checkJson = checkCheerioHtml($,checksfile);
+  action(checkJson);
+};
+
+/* Asynchronously reads file, and when done, checks
+ * it's contents using Cheerio, and then calls the
+ * call back on the results.
+ */
+var cheerioFile = function(htmlfile,checksfile,callback) {
+  fs.readFile(htmlfile,
+      function(err,result) {
+        if (err) throw err;
+        cheerioLoadAndCheck(result,checksfile,callback);
+      });
+	return;
+};
+
+/* Asynchronously reads from a URL, and when done, checks
+ * it's contents using Cheerio, and then calls the
+ * call back on the results.
+ */
+var cheerioUrl = function(URL,checksfile,callback) {
+	var res = restler.get(URL);
+	res.on('complete',function(result) {
+		if (result instanceof Error) {
+			sys.put('Error: ' + result.message);
+		} else {
+      cheerioLoadAndCheck(result,checksfile,callback);
+		}
+	});
+	return;
 };
 
 var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
 };
 
-var checkHtmlFile = function(htmlfile, checksfile) {
-    $ = cheerioHtmlFile(htmlfile);
+var checkCheerioHtml = function(C,checksfile) {
     var checks = loadChecks(checksfile).sort();
     var out = {};
     for(var ii in checks) {
-        var present = $(checks[ii]).length > 0;
+        var present = C(checks[ii]).length > 0;
         out[checks[ii]] = present;
     }
     return out;
+};
+
+var checkHtmlFile = function(htmlfile, checksfile) {
+    cheerioFile(htmlfile,checksfile,printJson);
+};
+var checkHtmlUrl = function(url, checksfile) {
+    cheerioUrl(url,checksfile,printJson);
 };
 
 var clone = function(fn) {
@@ -61,14 +120,28 @@ var clone = function(fn) {
     return fn.bind({});
 };
 
+/* Takes the following arguments:
+ * source -> the filename or the URL of the data source;
+ * getdataFn -> A (possibly asynchonous) function that gets data then calls callbackFn
+ * callbackFn -> function called after data is sourced.
+ */
+var checkSourceAndPrint = function(source,getdataFn,callbackFn) {
+	return getdataFn(source,callbackFn);
+};
+
 if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url>', 'URL to index.html', clone(assertURLExists), HTMLURL_DEFAULT)
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+		var checkJson;
+		if (program.url.length>0) {
+      checkHtmlUrl(program.url,program.checks);
+		} else {
+      checkHtmlFile(program.file,program.checks);
+		}
 } else {
     exports.checkHtmlFile = checkHtmlFile;
+    exports.checkHtmlUrl = checkHtmlUrl;
 }
